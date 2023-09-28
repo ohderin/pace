44ea127c941d6c5566404c5ebe40c9235bb47411
ohderin <contact@derin.io>
2023-09-27 20:28:39 -0500

    added functions

--- Assignments/Program1.java
+++ Assignments/Program1.java
@@ -88,8 +88,98 @@ class PaneraUser {
 public class Program1 {
 
     public static void main(String[] args) {
-            List<PaneraItem> menu = new ArrayList<>();
-            List<PaneraUser> user = new ArrayList<>();
+        List<PaneraItem> menu = new ArrayList<>();
+        List<PaneraUser> user = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        boolean exit = false;
+        System.out.println("1. Add Food Object to menu");
+        System.out.println("2. Add Beverage Object to menu");
+        System.out.println("3. Add Coffee Object to menu");
+        System.out.println("4. Add Panera User");
+        System.out.println("5. Order");
+        System.out.println("6. Bulk add menu items");
+        System.out.println("7. Print menu options");
+        System.out.println("8. Exit");
+
+        while (!exit) {
+            System.out.println("Enter a menu option: ");
+
+            int option = scan.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1: 
+                    addFood();
+                    break
+
+                case 2:
+                    addBeverage();
+                    break
+        
+                case 3:
+                    addCoffee();
+                    break
+
+                case 4:
+                    addUser();
+                    break
+
+                case 5:
+                    sendOrder();
+                    break
+
+                case 6:
+                    bulkAdd();
+                    break
+
+                case 7:
+                    System.out.println("1. Add Food Object to menu");
+                    System.out.println("2. Add Beverage Object to menu");
+                    System.out.println("3. Add Coffee Object to menu");
+                    System.out.println("4. Add Panera User");
+                    System.out.println("5. Order");
+                    System.out.println("6. Bulk add menu items");
+                    System.out.println("7. Print menu options");
+                    System.out.println("8. Exit");
+                    break
+                case 8:
+                    exit = true;
+                    System.out.println("Exiting...");
+                    break
+                default: 
+                    System.out.println("Invalid option");
+            }
+        }
+
+        private static void addFood() {
+            System.out.println("Enter name for Food item: ");
+            String name = scan.nextLine();
+            System.out.println("Enter item price: ");
+            double price = scan.nextDouble();
+            scan.nextLine();
+            System.out.println("Enter item side: ");
+
+        }
+
+        private static void addBeverage() {
+
+        }
+
+        private static void addCoffee() {
+
+        }
+
+        private static void addUser() {
+            
+        }
+
+        private static void sendOrder() {
+
+        }
+
+        private static void bulkAdd() {
+
+        }
 
 
     }
